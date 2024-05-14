bb328553e9435f38276a1bb85ddf72a495266c1a
Barwooo <152607868+Barwooo@users.noreply.github.com>
2024-05-14 08:27:27 +0000

    OOP

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     static void Main()
     {
-        // Here is your empty program!
-        Console.WriteLine("Hello world!");
+      SUB feed(BYREF state, BYREF size)
+      size  size + 1
+      OUTPUT "Fish fed" 
+      IF size = 5 THEN 
+      state  "FISH" 
+      ENDIF
+      ENDSUB
+ 
+   thisFishState  "Fish" 
+   thisFishSize  1
+   OUTPUT thisFishState, " is of size ", thisFishSize
+WHILE thisFishState <> "FISH"
+   feed(thisFishState, thisFishSize)
+ENDWHILE
+OUTPUT "It is now a big ", thisFishState
     }
 }
